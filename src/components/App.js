c9324771d284d0a8d1567c3392f3873c86850a7c
HaloZero <rdhaimade@lyft.com>
2016-11-20 16:12:05 -0800

    Sort alphabetically and remove unused code

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import '../index.css'
-import ValueList from './ValueList';
 import logo from './logo.png';
 import menuIcon from './menuIcon.png';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        values: []
-    };
-  }
-
-  componentDidMount() {
-    const host = 'https://fast-fjord-29570.herokuapp.com';
-    const url = `${host}/values`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(values => {
-        this.setState({
-            values: values.map( function(x) {
-                return {
-                    name: x.name,
-                    id: x.id
-                }
-            } ) 
-          });
-      })
-      .catch(error => {
-        console.error(error);
-    });
-  }
-
   render() {
 
     var headerStyle = {
--- src/components/ValueList.js
+++ src/components/ValueList.js
@@ -23,7 +23,12 @@ class ValueList extends Component {
                     name: x.name,
                     id: x.id
                 }
-            } ) 
+            }).sort(
+              function(a, b) { 
+                if(a.name < b.name) return -1;
+                if(a.name > b.name) return 1;
+                return 0;
+              }) 
           });
       })
       .catch(error => {
